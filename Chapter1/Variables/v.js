a44643c98ea3f664d6dc946f6f43b16be7e3384d
Pacho0405 <92822211+Pacho0405@users.noreply.github.com>
2021-11-08 21:18:13 -0500

    Add files via upload

---
+++ Chapter1/Variables/v.js
@@ -0,0 +1,7 @@
+var numero = prompt("ingrese un numero")
+var numero2 = prompt("ingrese otro numero")
+var suma = numero + numero2
+alert("La multiplicación de los dos numeros es"+( numero * numero2))
+alert("La resta de los dos numeros es " + ( numero - numero2))
+alert("La division de los dos numeros es "+ ( numero / numero2))
+alert("La suma de los dos numeros es " + suma )
