76eae844039146373a55f423d0354027635d63fd
Pacho0405 <92822211+Pacho0405@users.noreply.github.com>
2021-11-23 20:35:34 -0500

    Update script.js

--- punto2/script.js
+++ punto2/script.js
@@ -1,18 +1,33 @@
-let imc=Number(prompt("Ingrese su peso en kg"))
-if(imc<18.5)
+let imc = Number (prompt("ingrese su peso en Kg"))
+if(imc< 18.5)
 {
-    console.log("tu peso es bajo")
+    console.log("insuficiencia ponderal")
 }else{
-    if(imc<25)
-   {
-    console.log("normal")
-   }else{
-       if(imc<30)
-    {console.log("estás en preobesidad")
-   }
-         else{
-             if(imc>30)
-             console.log("estás obeso")
-            }
+    if(imc <= 24.9)
+    {console.log("intervalo normal")
+    }else{
+        if(imc <=25)
+        {console.log("sobrepeso")
+        }else{
+        if(imc <= 29.9)
+        {console.log("preobesidad")
+            }else{
+             if(imc <=30)
+             {console.log("obesidad")
+                }else{
+                if(imc <= 34.9 )
+                {console.log("obesidad de clase 1")
+                    }else{
+                     if(imc <= 39.9)
+                     {console.log("obesidad de clase 2")
+                        }else{
+                            if(imc >= 40)
+                            {console.log("obesidad de clase 3")
+                            }
+                        }
+                    }
+                }    
+             }   
+        }   
+    }
 }
-}   
